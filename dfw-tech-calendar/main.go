163c39b3fe6aba8ca0b9f585b05c5c65488779a0
Aditya Relangi <arelangi@users.noreply.github.com>
2018-12-31 09:33:17 -0600

    Revert "Updates (#8)"
    This reverts commit 81c7a32a7c07a9cf8cf9f638b03d32a2403eed01.

---
+++ dfw-tech-calendar/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/logzer0/meetupGCal"
+	"github.com/rk/go-cron"
+)
+
+func init() {
+	meetupGCal.ConfigFilePath = "keys_sample.json"
+	cron.NewDailyJob(3, 0, 0, func(time.Time) {
+		fmt.Println("Updating the calendar. Current time", time.Now())
+		meetupGCal.UpdateCalendar()
+	})
+}
+
+func main() {
+	fmt.Println("Updating the calendar. Current time", time.Now())
+	meetupGCal.UpdateCalendar()
+	http.ListenAndServe(":9580", nil)
+}
--- src/dfw-tech-calendar/vendor/gitlab.logzero.in/arelangi/mlog/README.md
+++
@@ -1,6 +0,0 @@
-mlog
-----
-
-mlog is a logging library that is a thin wrapper for [sirupsen/logrus](https://github.com/Sirupsen/logrus)
-
-
